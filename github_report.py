53424a5c4a9ab2b1f315dc88df078d8895a97aed
Hessam <hessam61@gmail.com>
2017-02-21 19:59:22 -0500

    Add chart tooltip and styling

--- github_report.py
+++ github_report.py
@@ -13,15 +13,31 @@ response = r.json()
 
 repo_dicts = response['items']
 
-names, stars = [], []
+names, plot_dicts = [], []
 for r in repo_dicts:
     names.append(r['name'])
-    stars.append(r['stargazers_count'])
+    plot_list = []
+    plot_dict = {
+        'value': int(r['stargazers_count']),
+        'label': str(r['description']),
+        }
+    plot_dicts.append(plot_dict)
 
 chart_style = ls('#333366', base_style=lcs)
 chart = pygal.Bar(style=chart_style, x_label_rotation=45, show_legend=False)
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=chart_style)
 chart.title = language.title() + ' projects on Github'
 chart.x_labels = names
 
-chart.add('', stars)
+chart.add('', plot_dicts)
 chart.render_to_file(language+'_repos.svg')
